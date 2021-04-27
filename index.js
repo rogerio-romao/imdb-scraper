83a8300c2909af8e45c9c0fe043dd43947e6d194
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-04-27 20:50:04 +0100

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const scraper = require('./scraper')
 
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.json({
